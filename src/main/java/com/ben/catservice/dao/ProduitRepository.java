1a86c63f1b86af25de95a505f208bc3ba16c71b6
BenCONDE <ben26_conde@yahoo.fr>
2020-01-03 14:28:34 +0100

    Ajout du composant nouveau produit

--- src/main/java/com/ben/catservice/dao/ProduitRepository.java
+++ src/main/java/com/ben/catservice/dao/ProduitRepository.java
@@ -2,6 +2,9 @@ package com.ben.catservice.dao;
 
 import com.ben.catservice.entities.Produit;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+@CrossOrigin("*")
+@RepositoryRestResource
 public interface ProduitRepository extends JpaRepository<Produit,Long> {
 }
